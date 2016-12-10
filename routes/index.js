d2282824d81b1e992799e2cf529748f05d387aa2
HYUK CHAN KWON <KHC@HYUKui-MacBook-Pro.local>
2016-12-11 04:32:20 +0900

    chanpion mainpage update

--- routes/index.js
+++ routes/index.js
@@ -1,21 +1,23 @@
 var express = require('express');
 var router = express.Router();
 
+var title = '찬피온 사이버 빌리지'
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '찬피온', subtitle: '소개' });
+  res.render('index', { title: title, subtitle: '소개' });
 });
 
 router.get('/members', function(req, res, next) {
-  res.render('members', { title: '찬피온', subtitle: '구성원' });
+  res.render('members', { title: title, subtitle: '구성원' });
 });
 
 router.get('/constitution', function(req, res, next) {
-  res.render('constitution', { title: '찬피온', subtitle: '헌법' });
+  res.render('constitution', { title: title, subtitle: '헌법' });
 });
 
 router.get('/history', function(req, res, next) {
-  res.render('history', { title: '찬피온', subtitle: '역사' });
+  res.render('history', { title: title, subtitle: '역사' });
 });
 
 module.exports = router;
