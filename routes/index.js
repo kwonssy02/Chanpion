cdca5d0cf19120b96a1759e531d54e560488c931
HYUK CHAN KWON <KHC@HYUKui-MacBook-Pro.local>
2016-12-11 01:42:31 +0900

    navbar 수정

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: '찬피온' });
+  res.render('index', { title: '찬피온', subtitle: '소개' });
+});
+
+router.get('/members', function(req, res, next) {
+  res.render('members', { title: '찬피온', subtitle: '구성원' });
+});
+
+router.get('/constitution', function(req, res, next) {
+  res.render('constitution', { title: '찬피온', subtitle: '헌법' });
 });
 
 module.exports = router;
